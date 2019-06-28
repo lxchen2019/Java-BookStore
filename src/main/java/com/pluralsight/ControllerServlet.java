d96e50babc625b7cd6d73f34aab58cd70feb97bb
Lixiong Chen <lixiong.chen@capgemini.com>
2019-06-28 21:18:23 +0200

    complete module 3

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -67,6 +67,9 @@ public class ControllerServlet extends HttpServlet {
 			case "/edit":
 				showEditForm(request, response);
 				break;
+			case "/update":
+				updateBook(request, response);
+				break;
 			default:
 				listBooks(request, response);
 				break;
@@ -144,5 +147,21 @@ public class ControllerServlet extends HttpServlet {
 		doGet(request, response);
 
 	}
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		String priceString = request.getParameter("bookprice");
+
+		Book newBook = new Book(id, title, author, Float.parseFloat(priceString));
+		
+		bookDAO.updateBook(newBook);
+		
+		response.sendRedirect("list");
+		
+	}
+	
 
 }
